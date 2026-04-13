eae2a0079b55204ddc0c0d00774a43e1fe8e0b6c
Artsiom Lukin <temnovid@gmail.com>
2026-04-13 14:41:02 +0200

    Add GitHub Actions workflow for CI/CD

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -17,6 +18,11 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
+
+    # AUTO-SWITCH: If running in GitHub Actions, force headless mode
+    if os.environ.get('CI') == 'true':
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
 
     # --- TOGGLE HEADLESS MODE ---
     # Comment out the line below if you want to see the browser window (Headed)
